6afcead8306f11e386803eab83466c45b2082eb6
josemiley02 <116444651+josemiley02@users.noreply.github.com>
2023-07-19 12:27:04 -0400

    Add files via upload

---
+++ Numeration System/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Numeration_System
+{
+    class Program
+    {
+        static void Main()
+        {
+            int newnumber = ParseInt("cbbaab", new char[] { 'a', 'b', 'c' });
+        }
+        static int ParseInt(string number, char[] bases)
+        {
+            int parse = 0;
+            for (int i = 0; i < number.Length; i++)
+            {
+                for (int j = 0; j < bases.Length; j++)
+                {
+                    if (number[i] == bases[j])
+                    {
+                        parse += (int)(j * Math.Pow(bases.Length, number.Length - 1 - i));
+                        break;
+                    }
+                }
+            }
+            return parse;
+        }
+    }
+}
